9a6cba2741d71f510e0c477af75632bb9ef0d173
SWASTIKGIT-debug <swastikchatterjee550@gmail.com>
2026-03-27 10:09:02 +0530

    optimized errors and application files

---
+++ source/application.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+const app = express();
+
+app.use(cors({
+    origin : process.env.cors_origin
+}))
+
+app.use(express.json())
+export {app}
--- source/index.js
+++ source/index.js
@@ -4,9 +4,11 @@ import dotenv from 'dotenv'
 
 import mongoose from "mongoose";
 import {DatabaseName} from "./constant.js";
-import Exp from "express";
+import {app} from "./application.js";
+import { error } from "node:console";
+import { exit } from "node:process";
 
-const app = Exp()
+// const app = Exp()
 console.log(process.env.PORT)
 console.log(`${process.env.mongoconnectionstring}/${DatabaseName}`)
 
@@ -16,12 +18,16 @@ dotenv.config()
 const databaseconnections = async () => {
     try {
         const DB_connection_res = await mongoose.connect(`${process.env.mongoconnectionstring}/${DatabaseName}`)
-        console.log(`${process.env.mongoconnectionstring}/${DatabaseName}`)
-        app.listen(process.env.PORT,()=>{
-            console.log(`connection successfull to ${process.env.PORT} port`)
-        })
+        console.log(`${process.env.mongoconnectionstring}/${DatabaseName}*****${DB_connection_res}`)
     } catch (error) {
         console.error(`there is an error ${error}`)
     }
 }
-databaseconnections() 
+databaseconnections().then(
+    app.listen(process.env.PORT,()=>{
+            console.log(`connection successfull to ${process.env.PORT} port`)
+        })
+).catch((error)=>{
+    console.log(error)
+    exit(1)
+})
